d42dc865ef363585096ecd1a4c1002342ec1cef9
Julian Sobott <julian.sobott@gmail.com>
2020-08-05 11:51:03 +0200

    Start DB

--- src/go.mod
+++ src/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	gitlab.com/golang-commonmark/markdown v0.0.0-20191127184510-91b5b3c99c19
 )
--- src/main.go
+++ src/main.go
@@ -4,9 +4,10 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+	"server/db"
 	"server/homepage"
-	"server/plugin_upload"
 	"server/login"
+	"server/plugin_upload"
 	"server/register"
 	"server/server"
 	"server/utils"
@@ -21,12 +22,15 @@ func main() {
 	register.Init(router)
 	plugin_upload.Init(router)
 
+	db.Open()
+	defer db.Close()
 
 	s := server.New(":8080", router)
 	err := s.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Server error: %v", err)
 	}
+
 }
 
 func serveStatic(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
