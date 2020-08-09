4bf4cacc8eead48d4fb69c5dee818ec450f00db0
Lukas Mendel <lukasmendel14043@gmail.com>
2020-08-09 12:32:47 +0200

    add go login logic

--- src/login/login.go
+++ src/login/login.go
@@ -3,6 +3,7 @@ package login
 //go:generate jade -pkg=views -writer -d ../views login.jade
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"server/views"
@@ -10,8 +11,25 @@ import (
 
 func Init(router *httprouter.Router) {
 	router.GET("/login", login)
+	router.POST("/userLogin", userLogin)
 }
 
 func login(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 	views.Login(writer)
+}
+
+func userLogin(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+
+	fmt.Printf("Funktion wird aufgerufen\n")
+
+	request.ParseForm()
+	user := request.Form.Get("userName")
+	pw := request.Form.Get("UserPassword")
+
+	fmt.Fprintf(writer, "User = %s\n", user)
+	fmt.Fprintf(writer, "Passwort = %s\n", pw)
+
+	fmt.Println("username:", request.Form["userName"])
+	fmt.Println("password:", request.Form["UserPassword"])
+
 }
