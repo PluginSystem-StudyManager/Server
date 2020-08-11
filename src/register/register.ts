7fa16dea589518143edaaf7a53f1956884e12261
Lukas Mendel <lukasmendel14043@gmail.com>
2020-08-11 20:33:35 +0200

    text fields are automatically colored if they are not filled in

--- src/register/register.ts
+++ src/register/register.ts
@@ -1,6 +1,9 @@
+
+const colorError = '#DC143C'
+
 function checkRegistration(form: HTMLFormElement) {
 
-    let colorError = '#DC143C'
+
     let errorField = document.getElementById("errorMessage")
     // @ts-ignore
     let data = new URLSearchParams(new FormData(form).entries())
@@ -8,13 +11,15 @@ function checkRegistration(form: HTMLFormElement) {
     let password1 = data.get('Password')
     let passwort2 = data.get('PasswordAgain')
 
+    allFieldsFilledOut()
+
     let result: boolean = comparePassword(password1, passwort2);
 
     if (result == false) {
-        console.log('Das War wohl nix')
 
         let pwField = document.getElementsByName('Password')
         pwField[0].style.backgroundColor = colorError
+
         let pwAgainField = document.getElementsByName('PasswordAgain')
         pwAgainField[0].style.backgroundColor = colorError
 
@@ -28,9 +33,8 @@ function checkRegistration(form: HTMLFormElement) {
         })
 
     }
-
-
 }
+
 
 function comparePassword(pwd: string, pwd2: string): boolean {
 
@@ -38,4 +42,25 @@ function comparePassword(pwd: string, pwd2: string): boolean {
     if (nummer == 0) {
         return true
     } else return false;
+}
+
+
+function allFieldsFilledOut(){
+
+    var fieldNames = ['FirstName', 'LastName', 'UserName', 'EMail', 'Password', 'PasswordAgain'];
+    fieldNames.forEach(fieldIsEmpty)
+
+}
+
+
+function fieldIsEmpty (value) {
+
+    let textField = <HTMLInputElement> document.getElementsByName(value)[0]
+    if (textField.value.length == 0){
+
+        textField.style.backgroundColor = colorError
+    }
+    else {
+        textField.style.backgroundColor = ''
+    }
 }
