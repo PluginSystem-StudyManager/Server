49f68e5567d5a3bbb8a8a04d414e9bec3a0f05d6
Julian Sobott <julian.sobott@gmail.com>
2020-08-12 15:56:39 +0200

    Update home.go

--- src/homepage/home.go
+++ src/homepage/home.go
@@ -3,6 +3,7 @@ package homepage
 //go:generate jade -pkg=views -writer -d ../views home.jade
 
 import (
+	"encoding/base64"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"server/db"
@@ -25,5 +26,45 @@ func Home(writer http.ResponseWriter, request *http.Request, _ httprouter.Params
 	if locNumPlugins > len(pluginsTemplateData) {
 		locNumPlugins = len(pluginsTemplateData)
 	}
-	views.Homepage(pluginsTemplateData[:locNumPlugins], writer)
+	header := buildHeaderData(request)
+	views.Homepage(header, pluginsTemplateData[:locNumPlugins], writer)
+}
+
+// TODO: Move to web-lib and use in all views
+type UserDate struct {
+	Token string	
+}
+// TODO: Move to web-lib and use in all views
+func buildHeaderData(r *http.Request) views.HeaderData {
+	cookie, err := r.Cookie("userdata") 	// TODO: constant cookie name
+	notLoggedIn := function() views.HeaderData {
+		return views.HeaderData {
+			Username ""
+			LoggedIn false
+		}
+	}
+	if err != nil {
+		// Can't find cookie
+		return notLoggedIn()
+	}
+	data, err := base64.StdEncoding.DecodeString(cookie.Value)
+	if err != nil {
+		// Wrong formatted Cookie
+		return notLoggedIn()	
+	}
+	var userData UserData
+	err = json.Unmarshal(data, &userData)
+	if err != nil {
+		// Wrong formatted cookie
+		return notLoggedIn()
+	}
+	user, err := db.UserByToken(userdata.Token) // TODO: implement in DB
+	if err != nil {
+		// Token does not exist or is expired. TODO: Maybe delete cookie
+		return notLoggedIn()	
+	}
+	return views.HeaderData{
+		Username user.Username
+		LoggedIn true
+	}
 }
